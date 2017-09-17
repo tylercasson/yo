9c9a42106f8f9770c6ab25cccb69d147e66c8939
Tyler Casson <me@tylercasson.com>
2017-09-17 18:35:44 -0500

    Initial commit

---
+++ yo/utils.py
@@ -0,0 +1,26 @@
+import argparse
+
+class Colors:
+    NORM = '\033[0m'
+    BOLD = '\033[1m'
+    SLIM = '\033[2m'
+    OBLIQUE = '\033[3m'
+    UNDERLINE = '\033[4m'
+    BLACK = '\033[30m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    
+    
+class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
+    def _format_action(self, action):
+        parts = super(argparse.RawDescriptionHelpFormatter, self)._format_action(action)
+        if action.nargs == argparse.PARSER:
+            parts = "\n".join(parts.split("\n")[1:])
+        return parts
+
+
